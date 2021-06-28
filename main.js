7d067ebab69c9c83c0e9499615305ec35ce6e824
Tdragon1539 <73697550+Tdragon1539@users.noreply.github.com>
2021-06-28 12:16:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(500, 500,);
+    canvas.position(300, 100);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 550, 400);
+  
+ fill(0, 230, 25);
+ rect(100, 40, 350, 30);
+ rect(45, 40, 30, 300);
+ rect(435, 40, 30, 300);
+ rect(100, 350, 350, 30);
+ fill(255, 0, 0);
+ circle(60, 60, 100);
+ circle(60, 350, 100);
+ circle(450, 60, 100);
+ circle(450, 350, 100);
+
+}
+take_snapshot();{
+    save('firstp5.png');
+}
